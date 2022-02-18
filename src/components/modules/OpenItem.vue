00a228fb818ca58abe8ea5b71cbaa5acd48e6cda
mirumi <info@milmemo.net>
2022-02-18 11:08:17 +0900

    [add] : add button

---
+++ src/components/modules/OpenItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="open_item_wrap">
+    
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+/**
+ * 
+ */
+
+
+
+
+
+</script>
+
+<style lang="scss" scoped>
+.open_item_wrap {
+
+}
+</style>
--- src/views/ZenbuHirakuView.vue
+++ src/views/ZenbuHirakuView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="zenbu_hiraku_wrap">
-    
+    <div class="item_wrap">
+      <OpenItem v-for="i in 5" :key="i" />
+    </div>
+    <div class="add_button">
+      <button type="button" class="button">
+        <SvgIcon icon="add" color="#646464" />
+        <span>追加する</span>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -8,6 +16,8 @@
 import { ref } from "vue"
 import router from "@/router/router"
 import { pagingInit } from "@/utils/utils"
+import SvgIcon from "@/components/parts/SvgIcon.vue"
+import OpenItem from "@/components/modules/OpenItem.vue"
 
 /**
  * paging init
@@ -17,10 +27,35 @@ pagingInit(router.currentRoute.value.name as string)
 
 
 
+
 </script>
 
 <style lang="scss" scoped>
 .zenbu_hiraku_wrap {
-
+  overflow-y: scroll;
+  .item_wrap {
+    margin-bottom: 30px;
+  }
+  .add_button {
+    margin: 20px auto;
+    text-align: center;
+    button {
+      position: relative;
+      padding: 0.6em 11em 0.57em;
+      background-color: #f0f0f0;
+      box-shadow: 1px 1px 2px 1px rgba(0, 0, 0, 0.13);
+      transition: 0.07s ease-out;
+      &:hover {
+        background-color: #e5e5e5;
+      }
+    }
+    span {
+      display: inline-block;
+      margin-top: 1.7px;
+      margin-left: 1.5em;
+      color: #646464;
+      font-weight: bold;
+    }
+  }
 }
 </style>
