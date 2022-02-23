15bad2ab7728c76e286ed4ad4549ebcdbecd8d37
mirumi <info@milmemo.net>
2022-02-23 12:52:44 +0900

    [remove] : vuex, axios

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
-import axios from 'axios'
 import App from './App.vue'
-import VueAxios from 'vue-axios'
 import router from './router/router'
 import Toaster from "@/components/modules/vue-toaster"
 import { createApp } from 'vue'
-import { key, store } from './store/store'
 
 const app = createApp(App)
   .use(router)
-  .use(store, key)
-  .use(VueAxios, axios)
 
 app.use(Toaster, {
   position: "bottom-right",
